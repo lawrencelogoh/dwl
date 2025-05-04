baba8349f7b2df63cab70185eeabd739a0ba4f95
Lawrence Logoh <lawrence.logoh@gmail.com>
2025-05-04 00:23:31 +0000

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+
+  buildInputs = [
+    pkgs.gnumake
+    pkgs.libinput
+    pkgs.pixman
+    pkgs.pkg-config
+    pkgs.wayland
+    pkgs.wayland-protocols
+    pkgs.wayland-scanner
+    pkgs.wlroots
+    pkgs.xwayland
+    pkgs.libxkbcommon
+  ];
+
+}
